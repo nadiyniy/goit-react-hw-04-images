e7d6d9b60ff46750cdddbdff80f19ce09dc31cb5
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-10-30 17:18:09 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { Button } from './button/Button';
 import { ImageGallery } from './imageGallery/ImageGallery';
 import { Modal } from './modal/Modal';
